321159419eb6f4ae0a879693d7d39497da6f9232
Matt Henry <greenideas@gmail.com>
2013-05-21 15:06:11 -0400

    refactor

--- spider.rb
+++ spider.rb
@@ -2,31 +2,52 @@ require 'nokogiri'
 require 'mechanize'
 require 'csv'
 
-agent = Mechanize.new
-page = agent.get("http://projects.latimes.com/homicide/day/monday/")
-homicides = []
+def get_names(day)
+  agent = Mechanize.new
+  page = agent.get("http://projects.latimes.com/homicide/day/#{day}/")
+  homicides = []
 
-#page.links_with(:href => /homicide\/post/).each do |l|
-  l = page.links_with(:href => /homicide\/post/)[0]
-  pg = l.click
+  puts "#{day}:"
+  page.links_with(:href => /homicide\/post/).each do |l|
+    begin
+      pg = l.click
+      puts "fetching #{pg.title.strip}. . ."
 
-  hom = {
-    :name => pg.search(".homicide-post h1").text.split(",")[0],
-    :date => pg.link_with(:href => /homicide\/date/).text.gsub(",",""),
-    :address => pg.search(".homicide-meta-rail div:first-child").text.strip,
-    :neighborhood => pg.link_with(:href => /homicide\/neighborhood/).text,
-    :age => pg.link_with(:href => /homicide\/age/).text,
-    :gender => pg.link_with(:href => /homicide\/gender/).text,
-    :dow => pg.link_with(:href => /homicide\/day/).text,
-    :jxn => pg.link_with(:href => /homicide\/jurisdiction/).text,
-    :race => pg.link_with(:href => /homicide\/race/).text
-  }
+      hom = {
+        :name => pg.search(".homicide-post h1").text.split(",")[0],
+        :date => pg.link_with(:href => /homicide\/date/).text.gsub(",",""),
+        :address => pg.search(".homicide-meta-rail div:first-child").text.strip,
+        :neighborhood => pg.link_with(:href => /homicide\/neighborhood/).text,
+        :age => pg.link_with(:href => /homicide\/age/).text,
+        :gender => pg.link_with(:href => /homicide\/gender/).text,
+        :dow => pg.link_with(:href => /homicide\/day/).text,
+        :jxn => pg.link_with(:href => /homicide\/jurisdiction/).text,
+        :race => pg.link_with(:href => /homicide\/race/).text
+      }
 
-  homicides << hom
-#end
+      homicides << hom
+    rescue => e
+      puts e
+    end
+  end
 
-CSV.open("homicides.csv", "wb") do |csv|
-  homicides.each do |h|
-    csv << [h[:name], h[:date], h[:address], h[:neighborhood], h[:age], h[:gender], h[:dow], h[:jxn], h[:race]]
+  write_csv homicides
+end
+
+def write_csv(homicides)
+  CSV.open("homicides.csv", "a+") do |csv|
+    homicides.each do |h|
+      csv << [ "#{h[:name]}",
+               "#{h[:date]}",
+               "#{h[:address]}",
+               "#{h[:neighborhood]}",
+               "#{h[:age]}",
+               "#{h[:gender]}",
+               "#{h[:dow]}",
+               "#{h[:jxn]}",
+               "#{h[:race]}" ]
+    end
   end
 end
+
+["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"].each { |day| get_names day }
